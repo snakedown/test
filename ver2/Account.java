a11d37d4e4a31aedd3825ffbda222d6867df0123
snakedown <cs.smith023@gmail.com>
2025-09-02 17:29:59 -0400

    Removed constructor

--- ver2/Account.java
+++ ver2/Account.java
@@ -5,10 +5,7 @@ public class Account {
 	private double balance; //New Comment
 	private String name;
 	
-	// Constructor
-	public Account(double balance) {
-		this("Unknown", balance);
-	}
+
 	
 	public Account(String name, double balance) {
 		this.name = name;
